3efe771361d2a0f781baa57c4b686a84fc00490a
phreaknik <johnboydiv@gmail.com>
2017-04-01 22:31:22 -0500

    Created boilerplate HTTPS GET and JSON parse data from poloniex

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 extern crate clap;
+extern crate hyper;
+extern crate hyper_rustls;
+extern crate serde_json;
 
+use std::io::Read;
 use clap::App;
+use hyper::Client;
+use hyper::header::Connection;
+use hyper::net::HttpsConnector;
+use serde_json::Value;
 
 fn main() {
     let cli_args = App::new(env!("CARGO_PKG_NAME"))
@@ -13,4 +21,20 @@ fn main() {
     let name = cli_args.value_of("name").unwrap_or("world");
 
     println!("Hello, {}!", name);
+
+    let client = Client::with_connector(HttpsConnector
+        ::new(hyper_rustls::TlsClient::new()));
+
+    let mut res = client.get("http://poloniex.com/public?command=return24hVolume")
+            .header(Connection::close())
+            .send()
+            .unwrap();
+
+    let mut msg = String::new();
+    res.read_to_string(&mut msg).unwrap();
+
+    let v: Value = serde_json::from_str(&msg.to_string()).unwrap();
+
+    println!("BTC volume: {}", v["BTC_ETH"]["BTC"]);
+    println!("ETH volume: {}", v["BTC_ETH"]["ETH"]);
 }
